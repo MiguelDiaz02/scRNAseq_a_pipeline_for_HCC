0a2df74d033bfc9422fbfd3667deae4497cdd57b
MiguelDiaz02 <g5diazcamposmiguel@gmail.com>
2025-09-30 12:11:07 -0600

    execute cv summary script

---
+++ python_scripts/disease_data_analysis/run_consensus.sh
@@ -0,0 +1,5 @@
+python /datos/home/mdiaz/HCC_project/HCC_analysis/consensus_cv_summary_UPDATED.py \
+  --cv_outdir /datos/home/mdiaz/HCC_project/HCC_analysis/Diseased_CV_2sc \
+  --export_prefix DiseasedCV_ \
+  --unknown_policy excl \
+  --collect_pred_scores true
