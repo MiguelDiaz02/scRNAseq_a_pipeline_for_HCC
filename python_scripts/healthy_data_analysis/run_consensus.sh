7da3b1cefb3d0f2119202e21a0b71f536b53ab3b
MiguelDiaz02 <g5diazcamposmiguel@gmail.com>
2025-09-30 12:20:31 -0600

    execute cv summary script

---
+++ python_scripts/healthy_data_analysis/run_consensus.sh
@@ -0,0 +1,5 @@
+python /home/mdiaz/HCC_project/HEALTHY_analysis/consensus_cv_summary_healthy.py \
+  --cv_outdir /home/mdiaz/HCC_project/Healthy_validation_analysis \
+  --fold_glob "fold*_HealthyCV" \
+  --export_prefix HealthyCV_ \
+  --collect_pred_scores true
